9ceb76edbd96369982d958cd897f86afd8c0920b
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-02-25 14:58:51 +0100

    added ability to check if switch is on or off

--- src/ts/ui/switch.ts
+++ src/ts/ui/switch.ts
@@ -1,11 +1,16 @@
+type SwitchElement = HTMLLabelElement & {on: boolean}
+
 const createSwitch = (str: string, id: string) => {
     const checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
     checkbox.id = id
-    const label = document.createElement('label')
+    const label = document.createElement('label') as SwitchElement
     label.classList.add('switch')
     label.htmlFor = id
     label.append(checkbox, str)
+    Object.defineProperty(label, 'on', {
+        get: () => checkbox.checked
+    })
     return label
 }
 
