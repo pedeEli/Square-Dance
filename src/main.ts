70cb74258bed6c24cf2d2f5da3eb6012407f04d4
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-03-02 04:52:26 +0100

    restructured the styles folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
 import '@css/main.css'
-import '@css/domino.css'
-import '@css/grid.css'
-import '@css/orangeBox.css'
-import '@css/btn.css'
-import '@css/switch.css'
 
 import {initControls} from '@ts/ui/controls'
 import {initState} from '@ts/state'
