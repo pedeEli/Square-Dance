7df8c59f32f1b7d85c653d9e605d63c415e6f515
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-03-02 02:10:34 +0100

    added size changing for mobile

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,14 @@ import '@css/switch.css'
 import {initControls} from '@ts/ui/controls'
 import {initState} from '@ts/state'
 
+const adjustLayout = () => {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const portrait = width < height
+    document.body.classList.toggle('vertical', portrait)
+}
+adjustLayout()
+window.addEventListener('resize', adjustLayout)
+
 initState()
 initControls()
